f5b3827a2a0cb390b42e9b2ab40c2445d72e2cf2
chrryyy <63435689+chrryyy@users.noreply.github.com>
2022-03-26 23:06:25 -0400

    required fields for forms

--- polls/forms.py
+++ polls/forms.py
@@ -6,3 +6,6 @@ class PersonForm(forms.ModelForm):
     class Meta:
         model = Person
         fields = ['group', 'name', 'duration', 'distance', 'image']
+    name = forms.CharField(required=True, widget=forms.TextInput(attrs={'required': 'true'}))
+    duration = forms.IntegerField(required=True, widget=forms.NumberInput(attrs={'required': 'true'}))
+    distance = forms.DecimalField(required=True, widget=forms.NumberInput(attrs={'required': 'true'}))
